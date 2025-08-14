4e0c74f40899a73ea82c0480e9e5dad61d799e7d
suleyman nurhussen <suleymanerziku@gmail.com>
2025-08-14 05:30:24 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,27 +2,32 @@
 import { initializeApp, getApp, getApps, FirebaseApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
-import { getAnalytics, Analytics } from "firebase/analytics";
+import { getAnalytics, Analytics, isSupported } from "firebase/analytics";
 
 const firebaseConfig = {
-  "projectId": "dagu-meet",
-  "appId": "1:697525767644:web:a984726480d4c388aed319",
-  "storageBucket": "dagu-meet.firebasestorage.app",
-  "apiKey": "AIzaSyCf1z9Yv0_RXZQkm_tRVgM2CS5koj0Edxc",
-  "authDomain": "dagu-meet.firebaseapp.com",
-  "measurementId": "G-Q01ETPHRJ0",
-  "messagingSenderId": "697525767644",
-  "databaseURL": "https://meetup-mobile-nsygc-default-rtdb.firebaseio.com"
+    apiKey: "AIzaSyCf1z9Yv0_RXZQkm_tRVgM2CS5koj0Edxc",
+    authDomain: "dagu-meet.firebaseapp.com",
+    projectId: "dagu-meet",
+    storageBucket: "dagu-meet.appspot.com",
+    messagingSenderId: "697525767644",
+    appId: "1:697525767644:web:a984726480d4c388aed319",
+    measurementId: "G-Q01ETPHRJ0",
+    databaseURL: "https://dagu-meet-default-rtdb.firebaseio.com"
 };
+
 
 // Initialize Firebase
 const app: FirebaseApp = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const auth = getAuth(app);
 const db = getDatabase(app);
 
-let analytics: Analytics;
+let analytics: Analytics | undefined;
 if (typeof window !== 'undefined') {
-  analytics = getAnalytics(app);
+    isSupported().then(supported => {
+        if (supported) {
+            analytics = getAnalytics(app);
+        }
+    })
 }
 
 export { app, auth, db, analytics };
